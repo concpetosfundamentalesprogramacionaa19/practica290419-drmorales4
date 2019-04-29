289a908c7d89963ae40e1d1e12efe0243d42e4f7
drmorales4 <davidmoralesec@gmail.com>
2019-04-29 16:46:43 -0500

    Practica 29_04_2019 en java

---
+++ Ejercicio29_04_19/src/descuentos/Descuento_cuidad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package descuentos;
+
+/**
+ *
+ * @author drmorales4
+ */
+public class Descuento_cuidad {
+     public static double descuento_cuidad = 0.20;
+     public static double descuento_edad = 0.10;
+     public static double descuento_estado = 0.05;
+     public static double descuento_cargas = 0.03;
+     
+     public static double tramites = 0.02;
+    
+}
